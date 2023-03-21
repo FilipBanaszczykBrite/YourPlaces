e9ca0c3516043db39a7d10f341f31dded518cabe
filip.banaszczyk <filipbanaszczyk1998@gmail.com>
2023-03-21 16:42:27 +0100

    init

--- force-app/main/default/lwc/yP_ResultApartments/yP_ResultApartments.js
+++ force-app/main/default/lwc/yP_ResultApartments/yP_ResultApartments.js
@@ -1,3 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 
-export default class YP_ResultApartments extends LightningElement {}
+export default class YP_ResultApartments extends LightningElement {
+    @api item;
+    @track imageSrc;
+    connectedCallback(){
+        console.log('connected')
+        this.imageSrc = "/sfc/servlet.shepherd/version/renditionDownload?rendition=THUMB720BY480&operationContext=CHATTER&versionId=" +
+        this.item.ContentDocumentId;
+    }
+}
--- force-app/main/default/lwc/yP_SearchResults/yP_SearchResults.js
+++ force-app/main/default/lwc/yP_SearchResults/yP_SearchResults.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import getApartments from '@salesforce/apex/YP_ProductSearchController.getApartments';
+//import getBusinessPremises from '@salesforce/apex/YP_ProductSearchController.getBusinessPremises';
+export default class YP_SearchResults extends LightningElement {
 
-export default class YP_SearchResults extends LightningElement {}
+    @track results = [];
+    connectedCallback(){
+        getApartments().then(result => {
+            console.log("Apartments " + JSON.stringify(result));
+            this.results = result;
+        })
+    }
+}
