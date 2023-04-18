3ad3e58eaf6969d5ccb8864b680566d8821e3724
filip.banaszczyk <filipbanaszczyk1998@gmail.com>
2023-04-18 12:52:24 +0200

    navigation to record page

--- force-app/main/default/lwc/yP_ResultBusinessPremises/yP_ResultBusinessPremises.js
+++ force-app/main/default/lwc/yP_ResultBusinessPremises/yP_ResultBusinessPremises.js
@@ -9,27 +9,37 @@ export default class YP_ResultBusinessPremises extends NavigationMixin(Lightning
     }
 
     navigateToRecordPage() {
-        if(this.isCommunity) {
-            this[NavigationMixin.Navigate]({
-                type: 'comm__namedPage',
-                attributes: {
-                    name: 'BusinessPremisesProductPage'
-                },
-                state: {
-                    recordId: this.product.Id
-                }
-            });
-        }
-        else{
-            this[NavigationMixin.Navigate]({
-                type: 'standard__recordPage',
-                attributes: {
-                    recordId: this.item.Id,
-                    objectApiName: 'Product2',
-                    actionName: 'view'
-                }
-            });
-        }
+        console.log('on click')
+        this[NavigationMixin.Navigate]({
+            type: 'comm__namedPage',
+            attributes: {
+                name: 'BusinessPremisesProductPage__c'
+            },
+            state: {
+                recordId: this.item.Id
+            }
+        });
+        // if(this.isCommunity) {
+        //     this[NavigationMixin.Navigate]({
+        //         type: 'comm__namedPage',
+        //         attributes: {
+        //             name: 'BusinessPremisesProductPage'
+        //         },
+        //         state: {
+        //             recordId: this.product.Id
+        //         }
+        //     });
+        // }
+        // else{
+        //     this[NavigationMixin.Navigate]({
+        //         type: 'standard__recordPage',
+        //         attributes: {
+        //             recordId: this.item.Id,
+        //             objectApiName: 'Product2',
+        //             actionName: 'view'
+        //         }
+        //     });
+        // }
         
     }
 }
