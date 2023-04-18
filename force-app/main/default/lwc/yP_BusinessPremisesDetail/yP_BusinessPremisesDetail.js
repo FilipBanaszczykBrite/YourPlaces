af9c51049f38b024182f52e10e93a2a1c90fd56e
filip.banaszczyk <filipbanaszczyk1998@gmail.com>
2023-04-18 13:22:48 +0200

    fetching details

--- force-app/main/default/lwc/yP_BusinessPremisesDetail/yP_BusinessPremisesDetail.js
+++ force-app/main/default/lwc/yP_BusinessPremisesDetail/yP_BusinessPremisesDetail.js
@@ -1,3 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track} from 'lwc';
+import getDetails from '@salesforce/apex/YP_BusinessPremisesController.getDetails';
+export default class YP_BusinessPremisesDetails extends LightningElement {
 
-export default class YP_BusinessPremisesDetails extends LightningElement {}
+    @api recordId;
+
+    @track record;
+
+    connectedCallback(){
+        console.log('id ' + this.recordId);
+        getDetails({recordId: this.recordId}).then(result => {
+            console.log('product details' + JSON.stringify(result));
+            this.record = result;
+        })
+    }
+}
