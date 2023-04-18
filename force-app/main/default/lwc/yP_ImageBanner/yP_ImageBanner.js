dd4cea1d55af2e8e428038f64172eea1b1326354
filip.banaszczyk <filipbanaszczyk1998@gmail.com>
2023-04-18 10:31:32 +0200

    init

---
+++ force-app/main/default/lwc/yP_ImageBanner/yP_ImageBanner.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import BANNER from "@salesforce/resourceUrl/YP_CommunityBanner";
+export default class YP_ImageBanner extends LightningElement {
+
+    imgSrc = BANNER;
+}
