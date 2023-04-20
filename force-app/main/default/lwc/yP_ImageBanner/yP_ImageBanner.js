42fade743aa1043a34d4d379db5083cfe580a860
filip.banaszczyk <filipbanaszczyk1998@gmail.com>
2023-04-20 12:37:41 +0200

    page layout changed

--- force-app/main/default/lwc/yP_ImageBanner/yP_ImageBanner.js
+++ force-app/main/default/lwc/yP_ImageBanner/yP_ImageBanner.js
@@ -1,6 +1,17 @@
 import { LightningElement } from 'lwc';
 import BANNER from "@salesforce/resourceUrl/YP_CommunityBanner";
+import { NavigationMixin } from 'lightning/navigation';
 export default class YP_ImageBanner extends LightningElement {
 
     imgSrc = BANNER;
+
+    goHome(){
+        console.log('go home')
+        this[NavigationMixin.Navigate]({
+            type: 'comm__namedPage',
+            attributes: {
+                pageName: 'Search_Results__c'
+            }
+        });
+    }
 }
--- force-app/main/default/lwc/yP_PhotoPreview/yP_PhotoPreview.js
+++ force-app/main/default/lwc/yP_PhotoPreview/yP_PhotoPreview.js
@@ -4,6 +4,4 @@ import { track, api } from 'lwc';
 export default class YP_PhotoPreview extends LightningModal {
 
     @api thumbnail;
-
-
 }
