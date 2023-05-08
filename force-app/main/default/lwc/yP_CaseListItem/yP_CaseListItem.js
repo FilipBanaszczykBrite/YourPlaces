3b7fc6f9465e0b08a94466a1bdebd3b0847ae222
filip.banaszczyk <filipbanaszczyk1998@gmail.com>
2023-05-08 15:15:17 +0200

    case cmp

---
+++ force-app/main/default/lwc/yP_CaseListItem/yP_CaseListItem.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class YP_CaseListItem extends LightningElement {}
---
+++ force-app/main/default/lwc/yP_CasesPage/yP_CasesPage.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class YP_CasesPage extends LightningElement {}
