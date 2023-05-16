0630f1af0908febc3ea98f8b6bc57200f6594404
Filip <filipbanaszczyk@wp.pl>
2023-05-16 20:33:12 +0200

    product searched fixed

--- force-app/main/default/lwc/yP_ResultBusinessPremises/yP_ResultBusinessPremises.js
+++ force-app/main/default/lwc/yP_ResultBusinessPremises/yP_ResultBusinessPremises.js
@@ -1,24 +1,40 @@
 import { LightningElement, api, track } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
+
 export default class YP_ResultBusinessPremises extends NavigationMixin(LightningElement) {
     @api item;
+    @api community;
     @track imageSrc;
 
     connectedCallback(){
-        this.imageSrc = "/sfc/servlet.shepherd/version/renditionDownload?rendition=THUMB720BY480&versionId=" +
-        this.item.DisplayUrl +
-        "&operationContext=CHATTER&contentId=" +
-        this.item.ContentDocumentId__c;
+        this.imageSrc =  this.item.DisplayUrl;
     }
 
     navigateToRecordPage() {
-        this[NavigationMixin.Navigate]({
-            type: 'standard__recordPage',
-            attributes: {
-                recordId: this.item.Id,
-                objectApiName: 'Product2',
-                actionName: 'view'
-            }
-        });
-}
+        console.log(this.item.id, this.community)
+        if(this.community == true){
+            this[NavigationMixin.Navigate]({
+                type: 'comm__namedPage',
+                attributes: {
+                    name: 'Business_Premise_Details__c'
+                },
+                state: {
+                    recordId: this.item.id
+                }
+            });
+        }
+        
+        else{
+           
+            this[NavigationMixin.Navigate]({
+                type: 'standard__recordPage',
+                attributes: {
+                    recordId: this.item.id,
+                    objectApiName: 'Product2',
+                    actionName: 'view'
+                }
+            });
+        }
+        
+    }
 }
